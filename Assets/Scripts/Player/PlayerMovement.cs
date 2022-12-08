059927312b77d7ec803e3463448ba84797f38c3b
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-12-08 07:53:18 +0600

    Commit 3

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 
 public class PlayerMovement : MonoBehaviour
 {
